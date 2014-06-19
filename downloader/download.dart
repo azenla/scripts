3ee68fe1a0d9c17c41225f87568ecf335749856d
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-19 15:22:17 -0400

    Initial Commit

---
+++ downloader/download.dart
@@ -0,0 +1,23 @@
+import "dart:io";
+import "package:http/http.dart" as http;
+
+var client = new http.Client();
+
+main(List<String> args) {
+  if (args.length != 2) {
+    print("Usage: download.dart <url> <path>");
+    exit(1);
+  }
+  var url = args[0];
+  var output = new File(args[1]);
+  
+  output.create()
+    .then((_) => client.get(url))
+    .then((response) {
+      if (response.statusCode != 200) {
+        print("ERROR: Failed to Download File: Status Code: ${response.statusCode}");
+        exit(1);
+      }
+      output.writeAsBytes(response.bodyBytes);
+    });
+}
